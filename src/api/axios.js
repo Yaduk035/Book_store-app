cfbbc7d4747cafa181728a0a67fe270849d7b1a4
Yadu Krishna <yaduk946@gmail.com>
2023-10-06 17:23:05 +0530

    changed axios url to hosted url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-const BASE_URL = "http://localhost:4000";
+const BASE_URL = "https://bookstore-backend-1wrk.onrender.com";
+// const BASE_URL = "http://localhost:4000";
 
 export default axios.create({
   baseURL: BASE_URL,
